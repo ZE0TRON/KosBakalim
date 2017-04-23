1029c3fcb881ada94af5450dd7c3c5fb8134680f
Batuhan Mete <batuhanmete67@gmail.com>
2017-04-24 01:48:13 +0300

    calisiyo mu la bu ? - User package created

---
+++ mobile/src/main/java/com/user/Appearances.java
@@ -0,0 +1,13 @@
+package com.user;
+
+/**
+ * Created by Batuhan on 24.04.2017
+ */
+
+abstract class Appearances {
+    protected int weight;
+    protected int height;
+    protected String gender;
+    protected int age;
+
+}
---
+++ mobile/src/main/java/com/user/User.java
@@ -0,0 +1,14 @@
+package com.user;
+
+/**
+ * Created by Batuhan on 24.04.2017
+ */
+
+class User extends Appearances{
+    private String email; //kullanici girisi emaille mi kullanici ismi ile mi olacak ?
+    private String password;
+    private boolean isSigned = false;
+    private String username; //diger insanlara gozukecek ad
+
+
+}
