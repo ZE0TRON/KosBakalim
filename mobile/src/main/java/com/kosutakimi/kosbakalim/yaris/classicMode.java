a42587e5c2d8d1fce34b8439a77158be8430fc4a
HaaaToka <okanalan@hacettepe.edu.tr>
2017-04-29 23:10:34 +0300

    ilk komit

---
+++ mobile/src/main/java/com/kosutakimi/kosbakalim/yaris/classicMode.java
@@ -0,0 +1,16 @@
+package com.kosutakimi.kosbakalim.yaris;
+
+/**
+ * Created by HaaToka on 29.4.2017.
+ */
+
+import com.kosutakimi.kosbakalim.user.User;
+
+public class classicMode {
+    //daha cok duzeltilecek yer var kodlara bakmayın ne yapacagıma dair hic bir fikrim yok
+
+    private void run(User a,User b){
+
+    }
+
+}
