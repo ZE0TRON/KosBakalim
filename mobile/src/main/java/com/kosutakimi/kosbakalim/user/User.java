b64d70d9ad65a240535bf98eb64994686ce678c1
HaaaToka <okanalan@hacettepe.edu.tr>
2017-04-30 00:47:06 +0300

    denem

--- mobile/src/main/java/com/kosutakimi/kosbakalim/user/User.java
+++ mobile/src/main/java/com/kosutakimi/kosbakalim/user/User.java
@@ -4,7 +4,9 @@ package com.kosutakimi.kosbakalim.user;
  * Created by Batuhan on 24.04.2017
  */
 
-class User extends Appearances{
+//batu bag !!!!  sinifini public yaptim kizma benim sifintan cagirmak icin
+
+public class User extends Appearances{
     private String email; //kullanici girisi emaille mi kullanici ismi ile mi olacak ?
     private String password;
     private boolean isSigned = false;
