57099c6a61f4d5ccd783ea057a098c09fee4ff6e
HaaaToka <okanalan@hacettepe.edu.tr>
2017-04-30 00:45:02 +0300

    yapilacaklar yazildi

--- mobile/src/main/java/com/kosutakimi/kosbakalim/yaris/classicMode.java
+++ mobile/src/main/java/com/kosutakimi/kosbakalim/yaris/classicMode.java
@@ -10,7 +10,29 @@ public class classicMode {
     //daha cok duzeltilecek yer var kodlara bakmayın ne yapacagıma dair hic bir fikrim yok
 
     private void run(User a,User b){
+        /*görsel arayuzdeki butonlar ready  lenmismi onu bekliycek
+        maksimum 30 sn ready e basma hakkın var
 
+        titresim + ses
+
+        veriler alinip ekrana yazilmaya baslancak
+        ikisi readyledikten sonra ilk 10 sn alistirma durumu
+
+        titresim + ses
+
+        clasicmod saniyesi geriye dogru akmaya baslıycak
+        **biri diğerini gectiginde sesli uyarı adam sni gecti yada sen gecince ondesin gibi
+
+        bittiginde sure titreme + ses
+
+        surekli veriler geldiginden her ceyrek saniyede bi kontrol edilip gecilip gecilmedigine bakılcak
+        buna gore veriler tutulacak en son veriler User lar uzerinden iletilecek
+        tutulan verilerden grafik olusturulacak
+
+        sonrada yarıs bitti ekranına gidecek kagıtta resmi var
+        */
+
+        System.out.println("girdim içerdeyim");
     }
 
 }
