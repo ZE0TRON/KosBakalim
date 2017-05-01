0095f3b97a17b5d2d54d3a4963b2b16dde6e26df
HaaaToka <okanalan@hacettepe.edu.tr>
2017-05-02 00:50:28 +0300

    little doing list

--- mobile/src/main/java/com/kosutakimi/kosbakalim/yaris/classicMode.java
+++ mobile/src/main/java/com/kosutakimi/kosbakalim/yaris/classicMode.java
@@ -1,14 +1,38 @@
 package com.kosutakimi.kosbakalim.yaris;
 
-
+import android.os.Vibrator;
 import com.kosutakimi.kosbakalim.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class classicMode {
     //daha cok duzeltilecek yer var kodlara bakmayın ne yapacagıma dair hic bir fikrim yok
 
+    private boolean isclickedReady(){ //parametre buton1 buton 2 gelsin
+        boolean flag1=false,flag2=false;
+        long start = System.currentTimeMillis();
+        long end = start + 20*1000; //  seconds * (1000) ms/sec
+        while (System.currentTimeMillis() < end)
+        {
+            // if(button1 click) flag1=true
+            //if(buton2 click) flag2=true
+        }
+        if(flag1 & flag2) return true;
+        return false;
+    }
+
     private void run(User a,User b){
+
+        long startTime,endTime;
+        int A_steps=0,B_steps=0;
+        List<Integer> A_Passed = new ArrayList<Integer>(); //bunun için bir queue yaparım cunku
+        List<Integer> B_Passed = new ArrayList<Integer>(); //array olusturunca 1024luk acıyor cok hafiza yiyo
+
+
         /*görsel arayuzdeki butonlar ready  lenmismi onu bekliycek
-        maksimum 30 sn ready e basma hakkın var
+        maksimum 20 sn ready e basma hakkın var
+        butona 1 kere clicklendikten sonra inaktif duruma geçsin
 
         titresim + ses
 
@@ -31,6 +55,49 @@ public class classicMode {
         unit testi ogren ve su lanet olasıca koco poponu kaldir artik vcs den push olmuyo lo :(
         sddfsf
         */
+
+        /*if(isclickedReady()){
+
+            Vibrator v = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);// Vibrate for 500 milliseconds  // xml den izin istegi
+
+            startTime = System.currentTimeMillis();
+            endTime = start + 10*1000;
+            while (System.currentTimeMillis() < end) //alistirma kismi
+            {
+                //ne yapılacak????
+            }
+
+            //baslıyoruz sesi
+            MediaPlayer mp = new MediaPlayer(this,R.raw...)
+            mp.start();
+
+            v.vibrate(500);
+
+            startTime = System.currentTimeMillis();
+            endTime = start + 50*1000;
+            User whoisLeader;
+            while (System.currentTimeMillis() < end) //alistirma kismi
+            {
+                A_steps= //oguzdan verileri cekecek
+                B_steps=
+                //titretme ekleyebiliriz birbirlerini gecince
+                if(whoisLeader!=A & A nin adım>B nin adım) A_passed.add(zamanı inte çevir) whoisLeader=A
+                if(whoisLeader!=B & B nin adım>A nin adım) B_passed.add(zamanı inte çevir) whoisLeader=B
+            }
+
+            //iki farklı kişiye iki farklı ekrana yönlendirilecek ogrenmek lazım
+            if(A_steps>B_steps) {A kazandı ekranına}
+            else if(A_steps<B_steps) {B kazandı ekranı}
+            else { beraberlik ekranı}
+
+        }
+
+           else{eşleşme ekranına geri dön}
+         */
+
+
+
 
         System.out.println("girdim içerdeyim");
     }
