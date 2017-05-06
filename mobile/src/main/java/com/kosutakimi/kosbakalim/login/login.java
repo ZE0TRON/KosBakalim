4f323ed426886213a4377cb7813e22eb2d240d94
Akif <mehmetakif.9825@gmail.com>
2017-05-06 16:10:05 +0300

    Login screen

---
+++ mobile/src/main/java/com/kosutakimi/kosbakalim/login/login.java
@@ -0,0 +1,15 @@
+package com.kosutakimi.kosbakalim.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kosutakimi.kosbakalim.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
